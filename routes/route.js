7a2fde71d755f2bf120ad7e8b2f4cc1e4ed6d25a
lakpafutilama <lakpa.lama4433@gmail.com>
2023-03-12 21:45:19 +0545

    updated

--- routes/route.js
+++ routes/route.js
@@ -6,19 +6,17 @@ const {checkOwnership} = require("../middleware/checkOwnership")
 //Get all data
 router.get('/:ownership', checkOwnership, getAllUsers )
 
-
 //Get data by username
 router.get('/:ownership/:username', checkOwnership, getIndUser);
 
 //post data
 router.post('/:ownership', checkOwnership, postUser);
 
-
 //delete by username
 router.delete('/:ownership/:username', checkOwnership, deleteUser)
 
-
 //update by username
 router.put('/:ownership/:username', checkOwnership, updateUser)
+
 
 module.exports = router;
