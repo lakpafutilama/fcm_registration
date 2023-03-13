84db742919d468d907e7a88d0a8c7d0124be81cd
lakpafutilama <lakpa.lama4433@gmail.com>
2023-03-13 20:37:34 +0545

    initial

--- app.js
+++ app.js
@@ -7,13 +7,14 @@ const app = express();
 const route = require('./routes/route.js')
 
 app.use(bodyParser.json());
-app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors());
 
 dbConnection
+
+app.use('/external/fcm/registration/', route);
 
 app.listen(3000, () => {
     console.log('server is started in port 3000');
 });
 
 
-app.use('/external/fcm/registration/', route);
