45b66a1887bbd551134cb28dd515422c31edb962
lakpafutilama <lakpa.lama4433@gmail.com>
2023-03-13 12:39:56 +0545

    final message

--- app.js
+++ app.js
@@ -2,12 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-const mongoose = require('./controller/db_connection');
 const app = express();
 const route = require('./routes/route.js')
 
 app.use(bodyParser.json());
-app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors({ origin: 'http://localhost:3000'}));
 
 app.listen(3000, () => {
     console.log('server is started in port 3000');
