ee1f83badc8c6dfdde7572f2d7f62885b519e0a2
lakpafutilama <lakpa.lama4433@gmail.com>
2023-03-16 11:41:23 +0545

    initial changes

--- controller/userController.js
+++ controller/userController.js
@@ -86,13 +86,12 @@ const updateUser = async (req, res) => {
     try {
         const user = await Model.findOne({ client_username: req.params.username })
         if (!user) {
-            data.save((err, doc) => {
-                if (err) {
-                    res.status(422).json(error("Duplication: registration_id or username already in use", res.statusCode))
-                } else {
-                    res.status(200).json(success("OK", `New user ${req.body.client_username} is registered.`, res.statusCode))
-                }
-            })
+            try {
+                const user = await Model.create(data);
+                res.json(success("OK", `New user ${req.body.client_username} is registered.`, res.statusCode));
+              } catch (err) {
+                res.status(422).json(error("Duplication: registration_id or username already in use", res.statusCode));
+              }
         } else {
             await Model.updateMany({ client_username: req.params.username }, { $set: data })
             res.json(success("OK", `${req.params.username} is updated.`, 200))
